3bc724a5b46f6407b600c2ba0dd3ce5d8de836e6
ghifarij <afga.ghifari@gmail.com>
2025-12-23 11:59:54 +0700

    fix job searching bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Script from "next/script";
 import { SessionProvider } from "@/context/sessionProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "RekJobs - Lowongan Kerja mudah dan ramah pengguna",
@@ -44,9 +33,7 @@ export default function RootLayout({
           strategy="beforeInteractive"
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <SessionProvider>
           <Navbar />
           {children}
